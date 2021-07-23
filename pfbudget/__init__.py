7cebb33898f81253f78ea237c3fb27bece5f776e
Luís Murta <luis@murta.dev>
2021-07-23 21:22:39 +0100

    Adds '__version__' and completes --version option

--- pfbudget/__init__.py
+++ pfbudget/__init__.py
@@ -1,3 +1,7 @@
+__all__ = ["run", "parse_data", "categorize_data"]
+__author__ = "Luís Murta"
+__version__ = "0.1"
+
 from .categories import categorize_data
 from .parsers import parse_data
 from .runnable import run
